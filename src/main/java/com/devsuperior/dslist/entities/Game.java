61f0882b12c352aa5b90d2b5004c3c10baa1f0f9
Claudio Cavalcanti <claudio.filho7@hotmail.com>
2023-07-14 23:55:25 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -15,28 +15,34 @@ public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String tilte;
+	private String title;
 	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String tilte, Integer year, String genre, String platforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
-		this.tilte = tilte;
+		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -50,12 +56,12 @@ public class Game {
 		this.id = id;
 	}
 
-	public String getTilte() {
-		return tilte;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setTilte(String tilte) {
-		this.tilte = tilte;
+	public void setTilte(String title) {
+		this.title = title;
 	}
 
 	public Integer getYear() {
@@ -80,6 +86,16 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
